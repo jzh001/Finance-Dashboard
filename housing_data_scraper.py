8fd2ab024b274f13ed825d2c1c62764d63e60cf6
Jiang Zhiheng <jiangzhsg@gmail.com>
2023-06-19 22:41:42 +0800

    no small step

--- housing_data_scraper.py
+++ housing_data_scraper.py
@@ -7,7 +7,7 @@ def getResaleHDBPrices():
     notOverlapping = True
     offset = 0
     res = []
-    step = 10
+    step = 5000
     while (notOverlapping):
         url = f'https://data.gov.sg/api/action/datastore_search?resource_id=f1765b54-a209-4718-8d38-a39237f502b3&limit={step}&sort=month%20desc&offset={offset}'
         
@@ -17,7 +17,6 @@ def getResaleHDBPrices():
         if res[-1]['_id'] <= len(df):
             notOverlapping = False
         offset += step
-        step = 5000
     df2 = pd.DataFrame(res).sort_values(by=['_id'])
     df2 = df2[df2['_id'] > len(df)].reset_index(drop=True)
     final_df = pd.concat([df, df2], ignore_index=True, sort=False).drop_duplicates(ignore_index=True)
